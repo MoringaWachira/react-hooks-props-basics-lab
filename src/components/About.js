efe0993b3224c883c82d4369970230ae975bef67
first-name second-name <silvia.wachira@student.moringa.com>
2022-09-27 13:07:00 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({bio,links}) {
+
+  // function aboutBio() {
+  //   if(!bio || bio === "") {
+  //     return null
+  //   } else {
+  //     return <p className="biolink"> {bio}</p>
+  //   }
+  // }
+
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {{bio}===undefined||{bio}.length===" "?null:(<p>{bio}</p>)}
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={links.github}  linkedin={links.linkedin} />
+
     </div>
   );
 }
+
 
 export default About;
