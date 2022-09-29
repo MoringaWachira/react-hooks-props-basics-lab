29c8de60d6b849f82b8583e9c4ca000c78d70ff9
first-name second-name <silvia.wachira@student.moringa.com>
2022-09-29 11:37:05 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -3,19 +3,19 @@ import Links from "./Links";
 
 function About({bio,links}) {
 
-  // function aboutBio() {
-  //   if(!bio || bio === "") {
-  //     return null
-  //   } else {
-  //     return <p className="biolink"> {bio}</p>
-  //   }
-  // }
+  function aboutBio() {
+    if(!bio || bio === "") {
+      return null
+    } else {
+      return <p className="biolink"> {bio}</p>
+    }
+  }
 
 
   return (
     <div id="about">
       <h2>About Me</h2>
-      {{bio}===undefined||{bio}.length===" "?null:(<p>{bio}</p>)}
+      {aboutBio()}
       
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
